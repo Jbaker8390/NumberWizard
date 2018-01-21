632cfa0f3e0f72ea8d429cb469b06c1a303630de
Jonathan <jonathan.baker@klearsystems.com>
2018-01-20 16:47:57 -0800

    added new prompt

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -7,6 +7,7 @@ public class NumberWizards : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("Welcome to Number Wizard");
+        print("Pick a number in your head, but don't tell me.");
 	}
 	
 	// Update is called once per frame
