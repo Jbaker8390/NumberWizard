2d0b6a9ba30d04eda403cb82622d0522afb16bbf
Jonathan <jonathan.baker@klearsystems.com>
2018-01-20 17:29:34 -0800

    added basic binary tree operation

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizards : MonoBehaviour {
+    //Global scope for var in this instance
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         print("Welcome to Number Wizard");
         print("Pick a number in your head, but don't tell me!");
 
-        int max = 1000;
-        int min = 0;
+     
 
         print("The highest number you can pick is " + max + ".");
         print("The lowest number you can choose is " + min + ".");
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess);
         print("Up = higher, down = lower, return = equal.");
 
        
@@ -27,12 +30,15 @@ public class NumberWizards : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             print("Up arrow pressed");
+            min = guess;
+            //basic binary tree operation
+            guess = (max + min) / 2;
         }else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             print("Down arrow pressed");
         }else if (Input.GetKeyDown(KeyCode.Return))
         {
-            print("Return pressed");
+            print("I won!");
         }
     }
 }
