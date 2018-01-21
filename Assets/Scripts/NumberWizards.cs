c5439cf7a5a57ba3da6d06b23405da0453017861
Jonathan <jonathan.baker@klearsystems.com>
2018-01-20 16:56:34 -0800

    added game basics

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -7,7 +7,17 @@ public class NumberWizards : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("Welcome to Number Wizard");
-        print("Pick a number in your head, but don't tell me.");
+        print("Pick a number in your head, but don't tell me!");
+
+        int max = 1000;
+        int min = 0;
+
+        print("The highest number you can pick is " + max + ".");
+        print("The lowest number you can choose is " + min + ".");
+
+        print("Is the number higher or lower than 500?");
+        print("Up = higher, down = lower, return = equal.");
+
 	}
 	
 	// Update is called once per frame
